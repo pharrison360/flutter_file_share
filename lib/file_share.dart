694e6dd050e31c10542ff6abce3617a5ac32b85d
pharrison360 <pharrison360@gmail.com>
2023-06-02 13:51:50 -0400

    Update file_share.dart to support null safety

--- lib/file_share.dart
+++ lib/file_share.dart
@@ -6,7 +6,7 @@ class FileShare {
   static const MethodChannel _channel = const MethodChannel('file_share');
 
   static Future<void> share(String pathToFile, String mimeType,
-      {String title, String text}) async {
+      {String? title, String? text}) async {
     await _channel.invokeMethod("shareFile", {
       "pathToFile": pathToFile,
       "mimeType": mimeType,
