74022cb40a9feabf7f674198870ac52279f47b14
pharrison360 <pharrison360@gmail.com>
2025-07-23 15:58:21 -0400

    Update FileSharePlugin.java for Flutter V2 embedding

--- android/src/main/java/com/evasilyev/flutter/file_share/FileSharePlugin.java
+++ android/src/main/java/com/evasilyev/flutter/file_share/FileSharePlugin.java
@@ -1,48 +1,75 @@
 package com.evasilyev.flutter.file_share;
 
+import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 
-import androidx.annotation.Nullable;
+import androidx.annotation.NonNull;
 import androidx.core.content.FileProvider;
 
 import java.io.File;
 
+import io.flutter.embedding.engine.plugins.FlutterPlugin;
+import io.flutter.embedding.engine.plugins.activity.ActivityAware;
+import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding;
 import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
+import io.flutter.plugin.common.PluginRegistry;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
-import io.flutter.plugin.common.PluginRegistry;
-import io.flutter.plugin.common.PluginRegistry.Registrar;
 
-/**
- * FileSharePlugin
- */
-public class FileSharePlugin implements MethodCallHandler, PluginRegistry.ActivityResultListener {
+/** FileSharePlugin */
+public class FileSharePlugin implements FlutterPlugin, MethodCallHandler, ActivityAware, PluginRegistry.ActivityResultListener {
 
     private static final int REQUEST_CODE = 1337;
     private static final String PROVIDER_EXTENSION = ".fileprovider";
 
-    /**
-     * Plugin registration.
-     */
-    public static void registerWith(Registrar registrar) {
-        final MethodChannel channel = new MethodChannel(registrar.messenger(), "file_share");
-        channel.setMethodCallHandler(new FileSharePlugin(registrar));
-    }
+    private MethodChannel channel;
+    private Context context;
+    private Activity activity;
+    private Result pendingResult;
 
-    private Registrar mRegistrar;
-    private Result mResult;
-
-    private FileSharePlugin(Registrar registrar) {
-        mRegistrar = registrar;
-        mRegistrar.addActivityResultListener(this);
+    @Override
+    public void onAttachedToEngine(@NonNull FlutterPluginBinding flutterPluginBinding) {
+        context = flutterPluginBinding.getApplicationContext();
+        channel = new MethodChannel(flutterPluginBinding.getBinaryMessenger(), "file_share");
+        channel.setMethodCallHandler(this);
     }
 
     @Override
-    public void onMethodCall(MethodCall call, Result result) {
-        mResult = result;
+    public void onDetachedFromEngine(@NonNull FlutterPluginBinding binding) {
+        channel.setMethodCallHandler(null);
+        channel = null;
+        context = null;
+    }
+
+    @Override
+    public void onAttachedToActivity(@NonNull ActivityPluginBinding binding) {
+        activity = binding.getActivity();
+        binding.addActivityResultListener(this);
+    }
+
+    @Override
+    public void onDetachedFromActivityForConfigChanges() {
+        activity = null;
+    }
+
+    @Override
+    public void onReattachedToActivityForConfigChanges(@NonNull ActivityPluginBinding binding) {
+        activity = binding.getActivity();
+        binding.addActivityResultListener(this);
+    }
+
+    @Override
+    public void onDetachedFromActivity() {
+        activity = null;
+    }
+
+    @Override
+    public void onMethodCall(@NonNull MethodCall call, @NonNull Result result) {
         if (call.method.equals("shareFile")) {
+            pendingResult = result;
             share(call, result);
         } else {
             result.notImplemented();
@@ -50,23 +77,24 @@ public class FileSharePlugin implements MethodCallHandler, PluginRegistry.Activi
     }
 
     @Override
-    public boolean onActivityResult(int requestCode, int resultCode, Intent intent) {
-        if (requestCode != REQUEST_CODE || mResult == null)
+    public boolean onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode != REQUEST_CODE || pendingResult == null)
             return false;
 
-        mResult.success(null);
+        pendingResult.success(null);
+        pendingResult = null;
         return true;
     }
 
     private void share(MethodCall call, Result result) {
         Args args = Args.fromCall(call);
-
         File fileToShare = new File(args.pathToFile);
+
         Uri fileUri;
         try {
             fileUri = FileProvider.getUriForFile(
-                    mRegistrar.context(),
-                    mRegistrar.activeContext().getPackageName() + PROVIDER_EXTENSION,
+                    context,
+                    context.getPackageName() + PROVIDER_EXTENSION,
                     fileToShare
             );
         } catch (IllegalArgumentException e) {
@@ -77,9 +105,10 @@ public class FileSharePlugin implements MethodCallHandler, PluginRegistry.Activi
         Intent shareIntent = new Intent(Intent.ACTION_SEND);
         shareIntent.putExtra(Intent.EXTRA_STREAM, fileUri);
         shareIntent.setType(args.mimeType);
+        shareIntent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
 
         Intent chooserIntent = Intent.createChooser(shareIntent, args.title);
-        mRegistrar.activity().startActivityForResult(chooserIntent, REQUEST_CODE);
+        activity.startActivityForResult(chooserIntent, REQUEST_CODE);
     }
 
     private static class Args {
@@ -87,10 +116,10 @@ public class FileSharePlugin implements MethodCallHandler, PluginRegistry.Activi
         final String pathToFile;
         final String mimeType;
 
-        private Args(String pathToFile, String mimeType, @Nullable String title) {
+        private Args(String pathToFile, String mimeType, String title) {
             this.pathToFile = pathToFile;
-            this.title = title;
             this.mimeType = mimeType;
+            this.title = title;
         }
 
         static Args fromCall(MethodCall call) {
@@ -100,7 +129,7 @@ public class FileSharePlugin implements MethodCallHandler, PluginRegistry.Activi
             return new Args(pathToFile, mimeType, title);
         }
 
-        private static<T> T getArg(MethodCall call, String argKey) {
+        private static <T> T getArg(MethodCall call, String argKey) {
             if (!call.hasArgument(argKey)) {
                 throw new IllegalArgumentException("Missing argument: " + argKey);
             }
